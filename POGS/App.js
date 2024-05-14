714c52c5f033ff310c363db91d93f84c22dfdfbd
Vivian Nguyen <nguyenmaivy999@gmail.com>
2024-05-14 13:35:03 +1000

    vivian_demo

--- POGS/App.js
+++ POGS/App.js
@@ -1,71 +1,89 @@
 import React, { useState } from "react";
-import { SafeAreaView, StyleSheet, Text, View, Button } from 'react-native';
+import { Image, Text, View, Button, SafeAreaView, ScrollView, StyleSheet } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
+import pic from "./assets/delivery_box.png";
 
-export default function POGSApp() {
-  // Data
-  const vegetables = ['Potato-$5', 'Carrot-$8', 'Cabbage-$5', 'Broccoli-$7', 'Spinach-$6'];
-  const fruits = ['Orange-$11', 'Blueberry-$12', 'Apple-$11', 'Grapes-$15', 'Pineapple-$14'];
+export default function App() {
+  const [veggieSelected, setVeggieSelected] = useState("");
+  const [veggieQuantity, setVeggieQuantity] = useState("1");
+  const [fruitSelected, setFruitSelected] = useState("");
+  const [fruitQuantity, setFruitQuantity] = useState("1");
+  const [totalPrice, setTotalPrice] = useState("0");
 
-  // States
-  const [selectedVeg, setSelectedVeg] = useState(vegetables[0]);
-  const [selectedFruit, setSelectedFruit] = useState(fruits[0]);
-  const [quantity, setQuantity] = useState('1');
-  const [totalCost, setTotalCost] = useState('Select items and quantity to see total cost');
-
-  // Calculate the total cost
   const calculateTotal = () => {
-    const vegPrice = parseInt(selectedVeg.split('-$')[1]);
-    const fruitPrice = parseInt(selectedFruit.split('-$')[1]);
-    const total = (vegPrice + fruitPrice) * parseInt(quantity);
-    setTotalCost(`Total Cost of Order: $${total}`);
+    const vegCost = veggieSelected ? parseInt(veggieSelected) : 0;
+    const vegQuant = parseInt(veggieQuantity);
+    const fruitCost = fruitSelected ? parseInt(fruitSelected) : 0;
+    const fruitQuant = parseInt(fruitQuantity);
+    setTotalPrice("Total cost of the order: $" + String((vegCost * vegQuant) + (fruitCost * fruitQuant)));
   };
 
   return (
     <SafeAreaView style={styles.container}>
-      <View>
-        <Text style={styles.heading}>Welcome to POGS Online Shopping App</Text>
-      </View>
-
-      <View style={styles.row}>
-        <Picker style={styles.picker}
-          selectedValue={selectedVeg}
-          onValueChange={(itemValue) => setSelectedVeg(itemValue)}
-        >
-          {vegetables.map((veg) => (
-            <Picker.Item key={veg} label={veg} value={veg} />
-          ))}
-        </Picker>
-
-        <Picker style={styles.picker}
-          selectedValue={selectedFruit}
-          onValueChange={(itemValue) => setSelectedFruit(itemValue)}
-        >
-          {fruits.map((fruit) => (
-            <Picker.Item key={fruit} label={fruit} value={fruit} />
-          ))}
-        </Picker>
-
-        <Picker style={styles.picker}
-          selectedValue={quantity}
-          onValueChange={(itemValue) => setQuantity(itemValue)}
-        >
-          {[...Array(5).keys()].map(i => (
-            <Picker.Item key={i} label={`${i + 1}`} value={`${i + 1}`} />
-          ))}
-        </Picker>
-      </View>
-
-      <View>
-        <Button title="Calculate" onPress={calculateTotal} />
-      </View>
-
-      <View>
-        <Text style={styles.heading}>{totalCost}</Text>
-      </View>
-
-      <View>
-        <Text style={styles.footer}>App developed by Group 8 </Text>
+      <ScrollView contentContainerStyle={styles.scrollView}>
+        <View style={styles.heading}>
+          <Text style={styles.title}>POGS ONLINE SHOPPING APP</Text>
+          <Image source={pic} style={styles.logo} />
+        </View>
+        <View style={styles.dropdownContainer}>
+          <Text style={styles.sectionTitle}>Vegetable Choice</Text>
+          <View style={styles.pickerRow}>
+            <Picker
+              style={styles.picker}
+              selectedValue={veggieSelected}
+              onValueChange={(itemValue) => setVeggieSelected(itemValue)}
+            >
+              <Picker.Item label="Select Vegetable" value="" />
+              <Picker.Item label="Tomato - $6" value="6" />
+              <Picker.Item label="Broccoli - $5" value="5" />
+              <Picker.Item label="Carrot - $5" value="8" />
+              <Picker.Item label="Spinach - $4" value="4" />
+            </Picker>
+            <Picker
+              style={styles.picker}
+              selectedValue={veggieQuantity}
+              onValueChange={(itemValue) => setVeggieQuantity(itemValue)}
+            >
+              <Picker.Item label="1" value="1" />
+              <Picker.Item label="2" value="2" />
+              <Picker.Item label="3" value="3" />
+              <Picker.Item label="4" value="4" />
+              <Picker.Item label="5" value="5" />
+            </Picker>
+          </View>
+          <Text style={styles.sectionTitle}>Fruit Choice</Text>
+          <View style={styles.pickerRow}>
+            <Picker
+              style={styles.picker}
+              selectedValue={fruitSelected}
+              onValueChange={(itemValue) => setFruitSelected(itemValue)}
+            >
+              <Picker.Item label="Select Fruit" value="" />
+              <Picker.Item label="Apple - $7" value="7" />
+              <Picker.Item label="Orange - $8" value="8" />
+              <Picker.Item label="Kiwi - $8" value="8" />
+              <Picker.Item label="Mang - $12" value="12" />
+            </Picker>
+            <Picker
+              style={styles.picker}
+              selectedValue={fruitQuantity}
+              onValueChange={(itemValue) => setFruitQuantity(itemValue)}
+            >
+              <Picker.Item label="1" value="1" />
+              <Picker.Item label="2" value="2" />
+              <Picker.Item label="3" value="3" />
+              <Picker.Item label="4" value="4" />
+              <Picker.Item label="5" value="5" />
+            </Picker>
+          </View>
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button title="Calculate Total" onPress={calculateTotal} color="#008037" />
+        </View>
+        <Text style={styles.totalPrice}>{totalPrice}</Text>
+      </ScrollView>
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>App developed by Group 8</Text>
       </View>
     </SafeAreaView>
   );
@@ -74,25 +92,66 @@ export default function POGSApp() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#E3D3E3'
+    backgroundColor: '#f7ffec',
+  },
+  scrollView: {
+    flexGrow: 1,
+    justifyContent: 'space-between',
+    padding: 20,
   },
   heading: {
-    fontSize: 20,
-    textAlign: 'center',
-    marginTop: 10,
-    marginBottom: 10
+    fontSize: 23, 
+    alignItems: 'center',
+    marginTop:10, 
+    marginBottom: 30,
   },
-  row: {
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#008037',
+    marginBottom: 20,
+  },
+  logo: {
+    width: 200,
+    height: 200,
+  },
+  dropdownContainer: {
+    marginBottom: 30,
+  },
+  sectionTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#008037',
+    marginBottom: 10,
+  },
+  pickerRow: {
     flexDirection: 'row',
-    marginBottom: 10
+    marginBottom: 20,
   },
   picker: {
-    flex: 1
+    flex: 1,
+    color: '#008037',
+    backgroundColor: '#e0f7da',
+    borderRadius: 10,
+    marginHorizontal: 5,
+  },
+  buttonContainer: {
+    alignItems: 'center',
+    marginBottom: 30,
+  },
+  totalPrice: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#008037',
+    textAlign: 'center',
   },
   footer: {
-    textAlign: 'center',
-    marginTop: 20,
-    fontSize: 12,
-    color: 'gray'
-  }
+    padding: 20,
+    backgroundColor: '#FDDDC7',
+    alignItems: 'center',
+  },
+  footerText: {
+    color: '#008037',
+    fontSize: 16,
+  },
 });
